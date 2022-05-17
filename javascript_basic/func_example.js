a84e6767f35ab651a936c4b616a230aaf1a2ad77
유관우 <skymill2000@gmail.com>
2022-05-17 11:35:53 +0900

    오브젝트 코드작성

--- javascript_basic/func_example.js
+++ javascript_basic/func_example.js
@@ -7,7 +7,19 @@ const plus = (p1, p2) => {
 }
 
 //#work 1 : 빼기, 나누기, 곱하기 기능을 선언 해볼것 
+const minus = (p1, p2) => {
+    return p1 - p2;
+}
 
+const mul = (p1, p2) => {
+    return p1 * p2;
+}
+
+const div = (p1, p2) => {
+    return p1 / p2;
+}
 
 console.log(plus(2, 5));
-console.log(plus(2, 9));
+console.log(minus(2, 5));
+console.log(mul(2, 5));
+console.log(div(2, 5));
---
+++ javascript_basic/object_example.js
@@ -0,0 +1,12 @@
+let car = {
+    name : "sonata",
+    ph : 500,
+    start : () => {
+        console.log('engine start');
+    },
+    stop : () =>{
+        console.log('engine stop');
+    }
+}
+
+console.log(car)
