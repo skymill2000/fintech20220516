73828847a0ac4ab1e5da63e085b40e8723884110
유관우 <skymill2000@gmail.com>
2022-05-17 11:49:39 +0900

    자바스크립트 배열 실습

---
+++ javascript_basic/array_example.js
@@ -0,0 +1,12 @@
+let array = ['a', 'b', 'c', 123];
+
+console.log(array)
+
+array.push('d');
+
+console.log(array)
+
+//in java
+//private String [] StringArray = new String[6];
+//private ArrayList<String> arrayListString = new ArrayList<String>;
+
