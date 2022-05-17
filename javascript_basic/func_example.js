6140f4453fabcf475ca5204796d6a26481e3b39b
유관우 <skymill2000@gmail.com>
2022-05-17 11:26:18 +0900

    #work1  minus, mul, div Function 추가

---
+++ javascript_basic/func_example.js
@@ -0,0 +1,13 @@
+// function plus(p1, p2) {
+//     return p1 + p2;
+// }
+
+const plus = (p1, p2) => {
+    return p1 + p2;
+}
+
+//#work 1 : 빼기, 나누기, 곱하기 기능을 선언 해볼것 
+
+
+console.log(plus(2, 5));
+console.log(plus(2, 9));
