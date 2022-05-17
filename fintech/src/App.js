2191c9e1cf1372bf44bce89d82b30970a55b484d
유관우 <skymill2000@gmail.com>
2022-05-17 15:15:38 +0900

    컴포넌트와 프롭스  프롭스를 통한 데이터 전달 실습

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+const element = <h1>hello</h1>;
+
+const Welcome = (props) =>{
+  console.log(props);
+  return <h2>안녕하세요 {props.username}님 나이는 {props.age}세 입니다</h2>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome username="유관우" age="33"></Welcome>
+        <Welcome username="유장비" age="32"></Welcome>
+        <Welcome username="유비" age="34"></Welcome>
       </header>
     </div>
   );
