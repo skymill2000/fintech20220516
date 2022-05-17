f56ea509d3383cd9b8f7acade641557f786d3060
유관우 <skymill2000@gmail.com>
2022-05-17 15:40:17 +0900

    StateAndEvent Component 추가  플러그인 설치 : React/Redux/React-Native snippets

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -7,8 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Welcome username="유관우" age="33"></Welcome>
-        <Welcome username="유장비" age="32"></Welcome>
-        <Welcome username="유비" age="34"></Welcome>
       </header>
     </div>
   );
---
+++ fintech/src/components/StateAndEvent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StateAndEvent = () => {
+  return (
+    <div>StateAndEvent</div>
+  )
+}
+
+export default StateAndEvent
