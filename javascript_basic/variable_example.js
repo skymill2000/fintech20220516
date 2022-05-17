94de63d2dbe3da92117be0d1da2eba7ff5f4ff1e
유관우 <skymill2000@gmail.com>
2022-05-17 11:14:00 +0900

    1. 변수 선언과 사용  - var , let, const 차이

---
+++ javascript_basic/variable_example.js
@@ -0,0 +1,25 @@
+//in java
+//private String name = "홍길동";
+//private int age = 34;
+//private double height = 182.5
+
+//javascript
+var uname = "홍길동";
+var age = 34;
+var hegiht = 13.23;
+
+console.log(uname, age, hegiht);
+
+let weight = 66;
+// let weight = 82;
+
+var age = 36;
+console.log(uname, age, hegiht);
+
+const gender = "F";
+
+// let hello = () => {
+//     console.log("hello");
+// }
+
+// hello();
