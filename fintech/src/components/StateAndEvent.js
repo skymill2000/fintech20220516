768d566bf0faeea8d534c6f0c611e707c6b121eb
유관우 <skymill2000@gmail.com>
2022-05-17 15:55:10 +0900

    이벤트 바인딩  변수를 사용한 데이터 변경

--- fintech/src/components/StateAndEvent.js
+++ fintech/src/components/StateAndEvent.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const StateAndEvent = () => {
+  
+  let userText = "사용자 입력 내용";
+
+  const handleClick = () => {
+      alert("test");
+  }
+
+  const handleChange = (event) => {
+    const {value} = event.target;
+    userText = value;
+    console.log(userText);
+  }
+
   return (
-    <div>StateAndEvent</div>
+    <>
+        <p>{userText}</p>
+        <input onChange={handleChange}></input>
+        <button onClick={handleClick}>데이터 변경</button>
+    </>
   )
 }
 
