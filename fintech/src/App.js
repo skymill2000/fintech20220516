a7e78b888b16e0d3ed6ac6ca3277138e0bbfc8a2
유관우 <skymill2000@gmail.com>
2022-05-18 13:29:41 +0900

    #work3 뉴스 검색 페이지 작성  - 이벤트 설정  - 프롭스 전달

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import ArrayMap from "./components/ArrayMap";
 import AxiosTest from "./pages/AxiosTest";
+import NewsApiPage from "./pages/NewsApiPage";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/a" element={<Welcome/>}></Route>
-        <Route path="/b" element={<ArrayMap/>}></Route>
+        <Route path="/news" element={<NewsApiPage/>}></Route>
         <Route path="/axiosTest" element={<AxiosTest/>}></Route>
       </Routes>
     </BrowserRouter>
--- fintech/src/pages/NewsApiPage.js
+++ fintech/src/pages/NewsApiPage.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppBar from '../components/common/AppBar'
+import SearchInput from '../components/news/SearchInput'
+import SearchResult from '../components/news/SearchResult'
 
 const NewsApiPage = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchList, setSearchList] = useState([]);
+  
+  const searchInputHandleChange = () => {
+    // 검색어를 변경합니다.
+  }
+
+  const searchButtonHandleClick = () => {
+    // 검색 버튼을 클릭합니다.
+    // searchInput의 데이터를 가지고와 Axios 요청 작성
+  }
+  
   return (
     <>
         <AppBar title={"뉴스검색"}></AppBar>
         {/* 검색어 입력 컴포넌트 */}
+        <SearchInput 
+            handleChange={searchInputHandleChange}
+            handleClick={searchButtonHandleClick}>
+        </SearchInput>
         {/* 검색 결과 출력 컴포넌트 */}
+        <SearchResult></SearchResult>
     </>
   )
 }
