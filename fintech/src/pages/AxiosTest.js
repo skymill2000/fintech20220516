24a8ac4f9006a23be92d6ead979bb8f79abb7137
유관우 <skymill2000@gmail.com>
2022-05-18 11:45:57 +0900

    뉴스 검색 어플리케이션 작성 실습 사전준비  컴포넌트 2개 추가

--- fintech/src/pages/AxiosTest.js
+++ fintech/src/pages/AxiosTest.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 const AxiosTest = () => {
   const [data, setData] = useState([]);
   const handleClick = () => {
-    axios.get('https://api.sampleapis.com/coffee/hot').then((response) => {
+    const url = "https://newsapi.org/v2/everything?q=tesla&from=2022-04-18&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5";
+    axios.get(url).then((response) => {
         console.log(response.data);
         setData(response.data)
     });
@@ -16,11 +17,6 @@ const AxiosTest = () => {
     <>
       <AppBar title={"통신테스트"}/>
       <button onClick={handleClick}>데이터 Request(요청)</button>
-      {
-          data.map((coffee) => {
-              return <p>{coffee.title}</p>
-          })
-      }
     </>
   )
 }
---
+++ fintech/src/pages/NewsApiPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from '../components/common/AppBar'
+
+const NewsApiPage = () => {
+  return (
+    <>
+        <AppBar title={"뉴스검색"}></AppBar>
+        {/* 검색어 입력 컴포넌트 */}
+        {/* 검색 결과 출력 컴포넌트 */}
+    </>
+  )
+}
+
+export default NewsApiPage
