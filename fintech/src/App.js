bcd45d749872ef6de55ad49e2cb68bb0a245865e
유관우 <skymill2000@gmail.com>
2022-05-18 10:32:42 +0900

    AxiosTest 페이지 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import ArrayMap from "./components/ArrayMap";
+import AxiosTest from "./pages/AxiosTest";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/a" element={<Welcome/>}></Route>
         <Route path="/b" element={<ArrayMap/>}></Route>
+        <Route path="/axiosTest" element={<AxiosTest/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ fintech/src/pages/AxiosTest.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AxiosTest = () => {
+  return (
+    <div>AxiosTest</div>
+  )
+}
+
+export default AxiosTest
