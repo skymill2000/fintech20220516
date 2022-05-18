b5498a4c73be6f032e52e3dd35f4859414765b81
유관우 <skymill2000@gmail.com>
2022-05-18 10:40:36 +0900

    엑시오스 테스트 페이지 추가

--- fintech/src/pages/AxiosTest.js
+++ fintech/src/pages/AxiosTest.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AppBar from '../components/common/AppBar';
 
 const AxiosTest = () => {
+  const [data, setData] = useState();
+  const handleClick = () => {
+    console.log('hello');
+  }  
+
   return (
-    <div>AxiosTest</div>
+    <>
+      <AppBar title={"통신테스트"}/>
+      <button onClick={handleClick}>데이터 Request(요청)</button>
+    </>
   )
 }
 
