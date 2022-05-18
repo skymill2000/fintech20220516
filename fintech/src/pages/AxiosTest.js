d504057de9a32c2b595174e7ea61ed4560f4d847
유관우 <skymill2000@gmail.com>
2022-05-18 11:03:10 +0900

    웹사이트 요청 주소 변경

--- fintech/src/pages/AxiosTest.js
+++ fintech/src/pages/AxiosTest.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import AppBar from '../components/common/AppBar';
+import axios from 'axios';
 
 const AxiosTest = () => {
   const [data, setData] = useState();
   const handleClick = () => {
-    console.log('hello');
+    axios.get('https://api.sampleapis.com/avatar/info').then((data) => {
+        console.log(data);
+    });
   }  
 
   return (
