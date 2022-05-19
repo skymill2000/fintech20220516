c88ddfe4831dde1537d260e1555620208c212a28
유관우 <skymill2000@gmail.com>
2022-05-19 15:31:43 +0900

    #work6 잔액 조회  - 잔액조회 기능 완성

--- fintech/src/pages/BalancePage.js
+++ fintech/src/pages/BalancePage.js
@@ -1,32 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 
 import BalanceCard from '../components/balance/BalanceCard'
 import AppBar from '../components/common/AppBar'
+import axios from 'axios';
 
 const BalancePage = () => {
   const { fintechUseNo } = queryString.parse(useLocation().search);
-
+  const accessToken = localStorage.getItem('accessToken');
+  const [balance, setBalance] = useState("0")
   //잔액 저장 스테이트
+
   useEffect(()=>{
     console.log(fintechUseNo)
-  })  
+    getBalance();
+  },[])  
 
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
-    let transId = "" + countnum; //이용기관번호 본인것 입력
+    let transId = "T991599190U" + countnum; //이용기관번호 본인것 입력
     return transId;
   };
   
   const getBalance = () =>{
       //계좌 잔액 조회 API 호출
+    const sendData = {
+        tran_dtime: "20220519143200",
+        bank_tran_id: genTransId(),
+        fintech_use_num: fintechUseNo
+    }
+
+    const option = {
+        method : "GET",
+        url: `/v2.0/account/balance/fin_num`,
+        headers: {
+            Authorization : `Bearer ${accessToken}`
+        },
+        params : sendData
+    }
+    
+    axios(option).then(({data}) => {
+        console.log(data);
+        setBalance(data);
+    })
   }
 
   return (
     <>
         <AppBar title={"잔액조회"}/>
-        <BalanceCard bankName={"테스트"} fintechNo={"테스트"} balance={"테스트"}></BalanceCard>
+        <BalanceCard bankName={balance.bank_name} fintechNo={balance.fintech_use_num} balance={balance.balance_amt}></BalanceCard>
     </>
   )
 }
