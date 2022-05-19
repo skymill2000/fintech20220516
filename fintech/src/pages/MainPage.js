2acd29b5f87b1b6a60cb4a307a212013c592a125
유관우 <skymill2000@gmail.com>
2022-05-19 13:46:57 +0900

    #work4 중간 힌트  - 엑시오스 요청 옵션 중간 완성  - 스테이트 추가

--- fintech/src/pages/MainPage.js
+++ fintech/src/pages/MainPage.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import AppBar from '../components/common/AppBar'
 
 const MainPage = () => {
+  const [accountList, setAccountList] = useState([]);
   const accessToken = localStorage.getItem('accessToken');
   const userSeqNo = localStorage.getItem('useSeqNo');
 
@@ -15,6 +17,24 @@ const MainPage = () => {
     // 사용자 데이터를 조회하는 AXIOS 요청을 작성 바랍니다.
     // hint : Method GET 때문에 요청하는 데이터는 option= { ... params : 에 데이터를 전달 }
     // token : headers : { Authorizaion : `bearer {여러분들의 토큰}` }
+
+    const sendData = {
+        //?
+    }
+
+    const option = {
+        method : "",
+        url: "",
+        headers: {
+            Authorization : ``
+        },
+        params: sendData 
+    }
+    
+    axios(option).then(({data}) => {
+
+    })
+
   }
 
   return (
