5d230d0583df87ee0f96a282c7268e1ea3e91332
유관우 <skymill2000@gmail.com>
2022-05-20 11:10:47 +0900

    npm i react-qr-reader --force  라이브러리 변경  - 해당 라이브러리 사용 부탁드립니다.

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -3,6 +3,7 @@ import AuthPage from "./pages/AuthPage";
 import AuthResultPage from "./pages/AuthResultPage";
 import AxiosTest from "./pages/AxiosTest";
 import BalancePage from "./pages/BalancePage";
+import Example from "./pages/Example";
 import MainPage from "./pages/MainPage";
 import NewsApiPage from "./pages/NewsApiPage";
 import QrCodePage from "./pages/QrCodePage";
@@ -21,6 +22,7 @@ function App() {
         <Route path="/balance" element={<BalancePage/>}></Route>
         <Route path="/qr" element={<QrCodePage/>}></Route>
         <Route path="/qrreader" element={<QrCodeReaderPage/>}></Route>
+        <Route path="/example" element={<Example/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- fintech/src/pages/QrCodeReaderPage.js
+++ fintech/src/pages/QrCodeReaderPage.js
@@ -1,48 +1,25 @@
 import React,{useState} from 'react'
 import AppBar from '../components/common/AppBar'
-import QrReader from 'react-web-qr-reader';
+import { QrReader } from 'react-qr-reader';
 
 const QrCodeReaderPage = () => {
-    const CustomStyles = {
-        overlay: {
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-          zIndex: "9",
-        },
-        content: {
-          width: "95%",
-          border: `0 solid black`,
-          top: "50%",
-          left: "50%",
-          right: "auto",
-          bottom: "auto",
-          marginRight: "-50%",
-          transform: "translate(-50%, -50%)",
-          zIndex: "99999",
-        },
-      };
-    
-    const [result, setResult] = useState('No result');
-    
-    const handleScan = (result) => {
-      if (result) {
-        setResult(result);
-      }
-    };
-    
-    const handleError = (error) => {
-      console.log(error);
-    };
+
+    const [result, setResult] = useState('')
+
+    const handleScan = (result, error) => {
+        if (!!result) {
+            setResult(result?.text);
+        }
+        if (!!error) {
+        }
+    }
     
     return(    
       <>
         <AppBar title={"QR 리더"}/>
-        <QrReader
-          delay={200}
-          style={CustomStyles}
-          onError={handleError}
-          onScan={handleScan}
-        />
-        <p>{result}</p>
+        <QrReader onResult={handleScan}/>
+      <p>{result}</p>
+
       </>
   )
 }
