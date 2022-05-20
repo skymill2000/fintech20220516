71595bc1eb9f18f9b283601d4fb42162f76af165
유관우 <skymill2000@gmail.com>
2022-05-20 11:33:43 +0900

    Modal 처리를 위한 라이브러리 추가  - npm install react-modal  - 모달 처리 코드 완료   : 카메라 QR 인식시 모달 활성화

--- fintech/src/pages/QrCodeReaderPage.js
+++ fintech/src/pages/QrCodeReaderPage.js
@@ -1,18 +1,42 @@
 import React,{useState} from 'react'
 import AppBar from '../components/common/AppBar'
 import { QrReader } from 'react-qr-reader';
+import Modal from 'react-modal';
 
 const QrCodeReaderPage = () => {
-
+    const CustomStyles = {
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          zIndex: "9",
+        },
+        content: {
+          width: "95%",
+          border: `0 solid black`,
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: "99999",
+        },
+    };
+    
     const [result, setResult] = useState('')
+    const [openModal, setOpenModal] = useState(false); //no camera : true
 
     const handleScan = (result, error) => {
         if (!!result) {
             console.log(result.text);
+            setOpenModal(true);
             setResult(result.text);
         }
         if (!!error) {
         }
+    }
+
+    const closeModal = () =>{
+        setOpenModal(false);
     }
     
     return(    
@@ -21,9 +45,16 @@ const QrCodeReaderPage = () => {
         <QrReader 
             onResult={handleScan}
             style={{ width: '100%' }}
+            scanDelay={1000}
         />
-      <p>{result}</p>
+        <p>{result}</p>
+        <Modal
+            isOpen={openModal}
+            style={CustomStyles}
+            onRequestClose={closeModal}
+        >
 
+        </Modal>
       </>
   )
 }
